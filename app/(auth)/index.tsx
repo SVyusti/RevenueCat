6cbb59a6f9e7e782fd607468caf2067cf496e8db
SVyusti <vyusti2083@gmail.com>
2026-02-05 11:15:35 +0530

    fix: access token support

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -10,19 +10,24 @@ import { auth } from '../../firebaseConfig';
 WebBrowser.maybeCompleteAuthSession();
 
 export default function Login() {
-  const redirectUri = AuthSession.makeRedirectUri();
-  console.log("REDIRECT URI:", redirectUri);
+//   const redirectUri = AuthSession.makeRedirectUri();
+//   console.log("REDIRECT URI:", redirectUri);
   const [request, response, promptAsync] = useAuthRequest({
     iosClientId: process.env.EXPO_PUBLIC_GOOGLE_IOS_CLIENT_ID!,
     androidClientId: process.env.EXPO_PUBLIC_GOOGLE_ANDROID_CLIENT_ID!,
     webClientId: process.env.EXPO_PUBLIC_GOOGLE_WEB_CLIENT_ID!,
-    redirectUri,
   });
 
   useEffect(() => {
-    if (response?.type === 'success') {
-      const { id_token } = response.params;
-      const credential = GoogleAuthProvider.credential(id_token);
+    if (response?.type === "success") {
+      const access_token = response.authentication?.accessToken;
+  
+      if (!access_token) {
+        console.error("Missing id_token",response);
+        return;
+      }
+  
+      const credential = GoogleAuthProvider.credential(null,access_token);
       signInWithCredential(auth, credential);
     }
   }, [response]);
